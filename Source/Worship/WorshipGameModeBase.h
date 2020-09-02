e145d194dba50c624810bcfcd7601c10158c1ed1
XRlab <>
2020-09-02 12:04:04 +0200

    Init commit. Added gitignore and lfs tracking

---
+++ Source/Worship/WorshipGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WorshipGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WORSHIP_API AWorshipGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
